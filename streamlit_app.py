0d623932595ea2c5807785206f014955b59b8881
Braahim <46976823+Braahim@users.noreply.github.com>
2022-09-26 11:56:29 +0200

    #3

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,7 @@ streamlit.header('First streamlit APP !')
 import pandas
 
 my_fruit_list = pandas.read_csv("fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:", list('my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
