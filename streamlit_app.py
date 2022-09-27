0be86e2b89ccd53d386fcd426b57a1e8bcbb81ab
Braahim <46976823+Braahim@users.noreply.github.com>
2022-09-27 14:08:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,3 @@
-
-import streamlit 
-streamlit.title('Hello')
-streamlit.header('First streamlit APP !')
-
-
-
-import pandas
-
-my_fruit_list = pandas.read_csv("fruit_macros.txt")
-
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruit_to_show = my_fruit_list.loc[fruit_selected]
-
-streamlit.dataframe(fruit_to_show)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
